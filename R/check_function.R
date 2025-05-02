96b3492a00ac84d90aeb43fb6cba8494f0a7452d
zzz13hj <hbracarense@hotmail.com>
2025-05-02 20:17:22 -0300

    Uploading documentation

--- R/check_function.R
+++ R/check_function.R
@@ -3,8 +3,6 @@
 #' Checks if there are any missing values in a given column from the dataset.
 #' @param name Column in which missing values will be sought.
 #' @param column Column name, in string format.
-#' @examples 
-#' DIETCOST::unique_values(DIETCOST::foods$food_id,"food ID");
 #' @export
 check_function <- function(name, column){
   if(any(is.na(name))){
